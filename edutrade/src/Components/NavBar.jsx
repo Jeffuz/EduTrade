b1e56814bfda5f0dbccaa45a58cfc08fa68e91a2
Simon Huang <huisimonliu@gmail.com>
2023-08-03 14:23:25 -0700

    Home Page basic layout done

--- edutrade/src/Components/NavBar.jsx
+++ edutrade/src/Components/NavBar.jsx
@@ -13,9 +13,9 @@ const NavBar = () => {
     }
     return (
         <div class='flex w-screen h-auto bg-slate-50 pr-[10vw]'>
-            <h3 class='m-auto ml-[5vw]'>EduTrade</h3>                
+            <h3 class='m-auto ml-[5vw] text-5xl'>EduTrade</h3>                
 
-            <div class='bg-red-50 p-10'>
+            <div class='bg-[#E9F7CA] p-10 text-xl'>
 
                 {user?.displayName ? (
                     <button onClick={handleSignOut}>Logout</button>
--- edutrade/src/Pages/Home Page.js
+++ edutrade/src/Pages/Home Page.js
@@ -12,10 +12,13 @@ export default function Home_Page() {
     return (
         <div>
             <NavBar/>
-            <SearchBarComponent/>
-            
-            <h3>Empowering Learning through Educational Exchange</h3>
-            <p>Find what students are selling locally and save</p>
+
+            <div class='bg-[#E9F7CA] p-36'>
+                <h3 class='mb-5 text-5xl'>Empowering Learning through Educational Exchange</h3>
+                <p class='mb-10 text-2xl text-[#F7D488] brightness-75'>Find what students are selling locally and save</p>
+                <SearchBarComponent/>             
+            </div>
+
             <div>
                 Contact Us Page
                 <ContactButton/>
