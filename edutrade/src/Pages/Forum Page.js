29e4518ae2caee3735f529de9bcf2e2150abd997
Mona Zhao <70651849+Mayuzh@users.noreply.github.com>
2023-08-04 17:53:11 +0800

    blog
    pop up window for posting

--- edutrade/src/Pages/Forum Page.js
+++ edutrade/src/Pages/Forum Page.js
@@ -1,32 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from '../Components/Post'; 
-// import PostList from '../Components/PostList'; 
 
 export default function Forum_Page() {
-    return (
-        <div className="h-screen flex flex-col px-4">
-            <hr className="h-px border-0 dark:bg-gray-400"></hr>
-            <div className="flex flex-1">
-                {/* Sidebar */}
-                <div className="w-1/4 p-4 border-r border-gray-400 flex flex-col">
-                    {/* Sidebar content */}
-                    Sidebar Content
-                    <div className="flex-grow"></div>
-                </div>
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-                {/* Main Content */}
-                <div className="w-3/4 p-4 overflow-y-auto">
-                    {/* Main content */}
-                    Main Content
-                    <div className="h-full">
-                        {/* Render the Post component */}
-                        <Post />
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-                        {/* Render the PostList component */}
-                        {/* <PostList /> */}
-                    </div>
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <div className="h-screen flex flex-col px-4">
+        <hr className="h-px border-0 dark:bg-gray-400"></hr>
+        <div className="flex flex-1">
+            {/* Sidebar */}
+            <div className="w-1/4 p-4 border-r border-gray-400 flex flex-col">
+                {/* Sidebar content */}
+                Sidebar Content
+                <div className="flex-grow"></div>
+            </div>
+
+            {/* Main Content */}
+            <div className="w-3/4 p-4 overflow-y-auto">
+                {/* Main content */}
+                Main Content
+                <div className="h-full">
+                    <button
+                        className="bg-blue-500 text-white px-4 py-2 rounded"
+                        onClick={openModal}
+                    >
+                        Create Post
+                    </button>
+                    {/* Render the PostList component */}
+                    {/* <PostList /> */}
+                    {/* <Post isOpen={isModalOpen} onClose={closeModal} /> */}
+                    <Post isOpen={isModalOpen} onClose={closeModal} />
                 </div>
             </div>
         </div>
-    );
+    </div>
+  );
 }
