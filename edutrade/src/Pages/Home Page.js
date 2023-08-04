34138eace39f819a5b275f78814f8ae32ca93de3
Jeffuz <jeffzhang0049@gmail.com>
2023-08-04 12:59:33 -0700

    Update Home Page.js

--- edutrade/src/Pages/Home Page.js
+++ edutrade/src/Pages/Home Page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchBarComponent from "../Components/SearchBarComponent";
+import { Link } from 'react-router-dom'
 
 export default function Home_Page() {
 
@@ -8,9 +9,11 @@ export default function Home_Page() {
             <div class='bg-[#E9F7CA] p-36'>
                 <h3 class='mb-5 text-5xl'>Empowering Learning through Educational Exchange</h3>
                 <p class='mb-10 text-2xl text-[#F7D488] brightness-75'>Find what students are selling locally and save</p>
-                <SearchBarComponent/>             
+                <SearchBarComponent />
+                <Link to="/forum" className='bg-white text-black hover:bg-gray-50 duration-300 ease-in-out shadow-lg w-[200px] rounded-md font-medium my-6 px-6 py-3'
+                >Go to Forum</Link>
             </div>
         </div>
-        
+
     )
 }
