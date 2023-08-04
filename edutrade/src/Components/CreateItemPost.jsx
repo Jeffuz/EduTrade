b12e51811dec5a5c3f6e930e4c1f487ddff0b40d
Simon Huang <huisimonliu@gmail.com>
2023-08-04 13:01:39 -0700

    Created Item component

---
+++ edutrade/src/Components/CreateItemPost.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CreateItemPost = () => {
+  return (
+    <div>
+      <form>
+        <label>Item Name</label>
+        <input type="text"/>
+
+        <label></label>
+      </form>
+    </div>
+  )
+}
--- edutrade/src/Pages/Product Details Page.js
+++ edutrade/src/Pages/Product Details Page.js
@@ -9,7 +9,13 @@ export default function Product_Details_Page() {
     }, []);
     return (
         <div className="bg-[#E9F7CA] text-center">
-            <SearchBarComponent/>
+            <div className="flex">
+                <div className="flex-1">
+                    <SearchBarComponent/>
+                </div>
+                <p className="m-auto flex-1">FILTERS</p>
+            </div>
+            
             {/* Display */}
             <div className=" flex flex-wrap justify-stretch bg-[#eeee] ml-[5vw] mr-[5vw] min-h-screen h-fit">
                 <ItemDisplay 
