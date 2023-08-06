38cb578e18bebe6141eb9341084e6c356bef70dd
Mona Zhao <70651849+Mayuzh@users.noreply.github.com>
2023-08-07 00:43:40 +0800

    thread page loaded
    use history for go back might be faster

--- edutrade/src/Pages/Thread Page.js
+++ edutrade/src/Pages/Thread Page.js
@@ -1,58 +1,48 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { firestore, firestoreCollection, firestoreGetDocs } from "../Firebase";
-import PostDetails from "../Components/PostDetails"; // Import the PostDetails component
+import { firestore, firestoreCollection, firestoreDoc, firestoreGetDoc } from "../Firebase"; // Adjust the import path as needed
+import { useParams, Link } from "react-router-dom";
 
 export default function ThreadPage() {
-  const { postId } = useParams(); // Get the postId from the URL
-  const [posts, setPosts] = useState([]);
-  const [selectedPost, setSelectedPost] = useState(null);
+  const { postId } = useParams();
+  const [post, setPost] = useState(null);
 
   useEffect(() => {
-    async function fetchPosts() {
+    async function fetchPost() {
       try {
-        const querySnapshot = await firestoreGetDocs(
-          firestoreCollection(firestore, "posts")
-        );
+        const docSnapshot = await firestoreGetDoc(firestoreDoc(firestore, `posts/${postId}`));
 
-        const postData = [];
-        querySnapshot.forEach((doc) => {
-          postData.push({ id: doc.id, ...doc.data() });
-        });
-
-        setPosts(postData);
+        if (docSnapshot.exists()) {
+          setPost(docSnapshot.data());
+        }
       } catch (error) {
-        console.error("Error fetching posts:", error);
+        console.error("Error fetching post:", error);
       }
     }
 
-    fetchPosts();
-  }, []);
+    fetchPost();
+  }, [postId]);
 
-  const handlePostClick = (postId) => {
-    const selected = posts.find((post) => post.id === postId);
-    setSelectedPost(selected);
-  };
-
+  if (!post) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div className="thread-page">
-      <div className="post-list">
-        <h2>Post List</h2>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id} onClick={() => handlePostClick(post.id)}>
-              {post.title}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="post-details">
-        {selectedPost ? (
-          <PostDetails postId={selectedPost.id} />
-        ) : (
-          <p>Select a post to view its details</p>
+    <div className="w-full p-5">
+       <Link to="/forum" className="bg-pink-500 py-2 px-4 text-white font-bold rounded focus:bg-pink-300">
+        Go Back
+      </Link>
+      <div className="bg-white p-4 rounded shadow-md">
+        {post.content && <p>{post.content}</p>}
+        {post.image && <img src={post.image} alt="Post Image" className="max-h-40 mb-2" />}
+        {post.link && (
+          <div className="break-words">
+            <a href={post.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+              {post.link}
+            </a>
+          </div>
         )}
       </div>
     </div>
   );
 }
+
+
