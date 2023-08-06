904dd7fd3b050a09505390d0c55c624527e3b0c4
Jeffuz <jeffzhang0049@gmail.com>
2023-08-05 18:14:39 -0700

    users are able to message publicly not privately

--- edutrade/src/Pages/Message Page.js
+++ edutrade/src/Pages/Message Page.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Messaging from "../Components/Messaging";
 
 export default function Message() {
     return (
         <div>
             Message Page
+            <Messaging/>
         </div>
     )
 }
